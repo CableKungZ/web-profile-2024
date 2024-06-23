7284fc12007aaf8cc62326e65d910eefc99985c0
CableKungZ <64020377+CableKungZ@users.noreply.github.com>
2024-06-23 15:28:24 +0700

    Update Skill.tsx

--- src/app/_components/Skill.tsx
+++ src/app/_components/Skill.tsx
@@ -20,32 +20,32 @@ const Skill = (props: Props) => {
       <Card title="Skill">
         <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4">
           <div className="text-center">
-            <FontAwesomeIcon icon={faPython} className="h-10" />
+            <FontAwesomeIcon icon={faPython} className="h-10 hover:fill-neutral-100	" />
             <p>Python</p>
           </div>
 
           <div className="text-center">
-            <FontAwesomeIcon icon={faReact} className="h-10" />
+            <FontAwesomeIcon icon={faReact} className="h-10 hover:fill-neutral-100	" />
             <p>React</p>
           </div>
           <div className="text-center">
-            <FontAwesomeIcon icon={faJava} className="h-10" />
+            <FontAwesomeIcon icon={faJava} className="h-10 hover:fill-neutral-100	" />
             <p>Java</p>
           </div>
           <div className="text-center">
-            <FontAwesomeIcon icon={faJs} className="h-10" />
+            <FontAwesomeIcon icon={faJs} className="h-10 hover:fill-neutral-100	" />
             <p>JS</p>
           </div>
           <div className="text-center">
-            <FontAwesomeIcon icon={faHtml5} className="h-10" />
+            <FontAwesomeIcon icon={faHtml5} className="h-10 hover:fill-neutral-100	" />
             <p>HTML5</p>
           </div>
           <div className="text-center">
-            <FontAwesomeIcon icon={faNode} className="h-10" />
+            <FontAwesomeIcon icon={faNode} className="h-10 hover:fill-neutral-100	" />
             <p>NodeJs</p>
           </div>
           <div className="text-center">
-            <FontAwesomeIcon icon={faSquareGithub} className="h-10" />
+            <FontAwesomeIcon icon={faSquareGithub} className="h-10 hover:fill-neutral-100	" />
             <p>Github</p>
           </div>
 
