0e33d455e204f942ffc3ec1f7611c732bc4589f7
CableKungZ <64020377+CableKungZ@users.noreply.github.com>
2024-06-23 10:50:57 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "John Farmer",
+  title: "Jassadakorn Suwannakoch",
   description: "Example personal website",
 };
 
